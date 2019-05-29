256b30532ca2eb2abfbc90997f59a8e6d37fa69c
Vasyl Sokolyk <vsokolyk@gmail.com>
2019-05-29 12:11:33 +0300

    Initial commit

---
+++ wp-performance-divi.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name:  wp-performance-divi
+Plugin URI:   https://github.com/Jazz-Man/wp-performance-divi
+Author:       Jazz-Man
+Author URI:   https://github.com/Jazz-Man
+*/
+
+
+new \JazzMan\PerformanceDivi\Divi();
